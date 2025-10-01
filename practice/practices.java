04e1bfffcb1ed82b790c4c0cf9a01339c819a227
pratyasha-dakshinray <pratyashaprakash@gmail.com>
2025-10-01 15:22:44 +0530

    Started new folder

---
+++ practice/practices.java
@@ -0,0 +1,22 @@
+/*21. Bitwise AND of 2 numbers */
+/*import java.util.Scanner;
+public class practices {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first number - ");
+        int num1 = input.nextInt();
+        System.out.println("Enter second number - ");
+        int num2 = input.nextInt();
+        int result = num1 & num2;
+        System.out.println("The bitwise and is - " + result);
+    }
+}*/
+
+/*22. Bitwise OR of 2 numbers */
+
+/*23. Bitwise XOR of 2 numbers */
+/*24. Bitwise compliment of 2 numbers */
+/*25. Bitwise use of left shift operator */
+/*26. Bitwise use of rght shift operator */
+/*27. To check if a number is even or odd using bit wise */
